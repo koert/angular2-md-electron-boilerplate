46264deda38918487ecb26ac229e0ad9afd4cac2
Koert Zeilstra <koert@zencode.nl>
2016-11-18 16:20:32 +0100

    added material dependency

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { MdToolbar } from '@angular/material';
+const ipcRenderer = require('electron').ipcRenderer;
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+
+
+  constructor() {
+    ipcRenderer.on("reply", (event, arg) => {
+      console.log("Reply was " + arg);
+    });
+  }
+
+  public text_hello_world: string = "Hello Angular 2!";
+  public text_small: string = "Greatness awaits..."
+
+  public test(): void {
+    console.log("Getestet");
+    ipcRenderer.send("message", "tested");
+  }
+
+}
