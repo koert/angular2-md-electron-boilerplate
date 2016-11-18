4bf56b0542a74b22bf26821927b37fcf7898e1cd
Koert Zeilstra <koert@zencode.nl>
2016-11-18 19:23:41 +0100

    Added tabs and snackbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MdToolbar } from '@angular/material';
+import {MdSnackBar, MdSnackBarConfig} from '@angular/material';
+
 const ipcRenderer = require('electron').ipcRenderer;
 
 @Component({
@@ -10,18 +12,22 @@ const ipcRenderer = require('electron').ipcRenderer;
 export class AppComponent {
 
 
-  constructor() {
+  constructor(public snackBar: MdSnackBar) {
     ipcRenderer.on("reply", (event, arg) => {
       console.log("Reply was " + arg);
     });
   }
 
-  public text_hello_world: string = "Hello Angular 2!";
-  public text_small: string = "Greatness awaits..."
+  public title: string = "Hello Angular 2!";
+  public message: string = "Greatness awaits..."
 
   public test(): void {
     console.log("Getestet");
     ipcRenderer.send("message", "tested");
+
+    let config = new MdSnackBarConfig();
+    // config.duration = 0;
+    this.snackBar.open(this.message, null, config);
   }
 
 }
